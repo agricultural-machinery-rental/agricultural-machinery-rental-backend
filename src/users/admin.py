3a6079d00a609a34e7e80222753af0305ccdfe27
Pavel Gaidukov <mailgpn@gmail.com>
2023-07-20 19:53:00 +0500

    fix: absolute file import path admin.py

--- src/users/admin.py
+++ src/users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 
-from .models import User
+from users.models import User
 
 
 @admin.register(User)
