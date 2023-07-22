8ab9e1d9e84967e467bb8a6d25e277ec7bc77c65
Alexey Sprygin <ASprygin@beeline.ru>
2023-07-22 16:41:52 +0300

    test black with ckeck and diff args.

--- src/orders/apps.py
+++ src/orders/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class OrdersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "orders"
+
+test_black_with_long_string = ["jfhdfdsf", "django.db.models", "django", "django",  "django", "django"]
+
