5013b8ca161425c0b01dae329874837d1aac5cc8
Kirill Sobolev <sblvkr@gmail.com>
2023-08-07 20:45:36 +0300

    first

--- src/api/v1/machineries/views.py
+++ src/api/v1/machineries/views.py
@@ -1,4 +1,8 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from api.v1.machineries.serializers import MachinerySerializer
 from machineries.models import Machinery
@@ -13,3 +17,38 @@ class MachineryViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = Machinery.objects.all()
     serializer_class = MachinerySerializer
+
+    @action(
+        detail=True,
+        methods=("post", "delete"),
+        url_path="favorite",
+    )
+    def favorite(self, request, *args, **kwargs):
+        machinery = get_object_or_404(Machinery, pk=kwargs["pk"])
+        queryset = Machinery.objects.filter(
+            user=request.user, machinery=machinery
+        )
+        if request.method == "POST":
+            if queryset.exists():
+                return Response(
+                    {
+                        "message": f"Такой объект уже добавлен в "
+                        f"{Machinery._meta.verbose_name.title()}"
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            serializer = MachinerySerializer(
+                machinery, context={"request": request}
+            )
+            Machinery.objects.create(user=request.user, machinery=machinery)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if not queryset:
+            return Response(
+                {
+                    "message": f"Такой объект отсутствует в "
+                    f"{Machinery._meta.verbose_name.title()}"
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        queryset.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- src/machineries/models.py
+++ src/machineries/models.py
@@ -118,4 +118,4 @@ class Favorite(models.Model):
         ]
 
     def __str__(self) -> str:
-        return f"{self.user} добавил {self.machinery.name}"
+        return f"{self.user} добавил {self.machinery.machinery.name}"
