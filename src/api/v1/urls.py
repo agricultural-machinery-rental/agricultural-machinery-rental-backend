77e5f66a35461cac9be0461eaf0112ae64ec3c70
Kirill Sobolev <sblvkr@gmail.com>
2023-08-09 21:31:06 +0300

    merge from develop

--- src/api/v1/urls.py
+++ src/api/v1/urls.py
@@ -1,6 +1,32 @@
 from django.urls import include, path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="AgroBooking API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path(
+        "swagger<format>/",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
     path("machineries/", include("api.v1.machineries.urls")),
     path("orders/", include("api.v1.orders.urls")),
     path("users/", include("api.v1.users.urls")),
