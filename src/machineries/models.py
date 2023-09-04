5001abcb5a0a7a14e0dc5bf45a27fedc171a7ca1
Kirill Sobolev <sblvkr@gmail.com>
2023-09-04 13:58:30 +0300

    add endpoint top

--- src/machineries/models.py
+++ src/machineries/models.py
@@ -109,6 +109,10 @@ class Machinery(models.Model):
     price_per_hour = models.DecimalField(
         verbose_name="Цена аренды в час", max_digits=7, decimal_places=2
     )
+    count_orders = models.PositiveIntegerField(
+        verbose_name="Количество заказов",
+        default=0,
+    )
 
     objects = MachineryManager()
 
