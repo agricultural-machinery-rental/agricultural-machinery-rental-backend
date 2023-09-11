c2a5743e77f3b43368ca8aefa45c22a451a5f5df
Kirill Sobolev <sblvkr@gmail.com>
2023-09-11 11:14:01 +0300

    add test

--- src/orders/tests/test_models.py
+++ src/orders/tests/test_models.py
@@ -1,18 +1,26 @@
 from datetime import timedelta
 from django.utils import timezone
 
+from config.settings import PREFIX_ORDER_NUMBER
 from core.fixtures import TestOrdersFixture
 from orders.models import Reservation
 
 
 class TestOrders(TestOrdersFixture):
     def test_order_creation(self):
-        order = Reservation.objects.create(
+        reservation = Reservation.objects.create(
             machinery=self.machinary_2,
             renter=self.user,
             start_date=timezone.now() + timedelta(minutes=15),
             end_date=timezone.now() + timedelta(hours=20),
         )
         self.assertTrue(
-            Reservation.objects.filter(number=order.number).exists()
+            Reservation.objects.filter(number=reservation.number).exists()
         )
+
+        year = timezone.now().strftime("%Y")
+        self.assertTrue(reservation.number.startswith("AG" + year))
+        self.assertTrue(reservation.number[len("AG" + year) :].isdigit())
+
+        expected_number = f"{PREFIX_ORDER_NUMBER}{year}000005"
+        self.assertEqual(reservation.number, expected_number)
