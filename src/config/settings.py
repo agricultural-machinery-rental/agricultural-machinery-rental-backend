4167e2cb8ba294f5fb09c537adc7b66d81276305
Andrey Klevtsov <andrewklevt@gmail.com>
2023-09-12 19:29:53 +0300

    fix: replace logger conf to log_config

--- src/config/settings.py
+++ src/config/settings.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 from pathlib import Path
 
 from dotenv import load_dotenv
+from .log_config import *
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -108,50 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-LOG_FILE = os.path.join(BASE_DIR, "../django.log")
-REQUESTS_FILE = os.path.join(BASE_DIR, "../api_requests.log")
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter": "simple",
-        },
-        "file": {
-            "level": "WARNING",
-            "class": "logging.FileHandler",
-            "filename": LOG_FILE,
-            "formatter": "main",
-        },
-        "request": {
-            "level": "INFO",
-            "class": "logging.FileHandler",
-            "filename": REQUESTS_FILE,
-            "formatter": "main",
-        },
-    },
-    "formatters": {
-        "main": {
-            "format": " %(asctime)s, %(levelname)s,"
-            " %(message)s, %(name)s, %(funcName)s,"
-            " %(lineno)d,",
-        },
-        "simple": {
-            "format": "%(log_color)s%(levelname)s,"
-            " %(message)s, %(name)s, %(funcName)s,",
-            "()": "colorlog.ColoredFormatter",
-        },
-    },
-    "loggers": {
-        "": {
-            "handlers": ["console", "file", "request"],
-            "level": "DEBUG",
-            "propagate": True,
-        },
-    },
-}
 
 LANGUAGE_CODE = "ru-RU"
 
